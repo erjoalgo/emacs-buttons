3c138153cc231835885f9c67f3b3b433a30fc77a
Ernesto Alfonso <erjoalgo@gmail.com>
2017-04-04 14:16:54 -0700

    add buttons.el refactoring

---
+++ lisp/core/buttons.el
@@ -0,0 +1,105 @@
+(defun buttons-add-super-modifier (keyspec)
+  (if (equal keyspec "\\")
+      (kbd "s-\\")
+    (let ((fmt
+	   (if (equal (length keyspec) 1) "(kbd \"s-%s\")"
+	     "(kbd \"<s-%s>\")")))
+      (eval (read (format fmt keyspec))))))
+
+(defun buttons-read-action (action)
+  (destructuring-bind (fun . args) action
+    (let ((arg1 (car args)))
+					;(edebug)
+      (case fun
+	('ins `(,arg1 (insert ,arg1)))
+	('rec `("REC" (buttons-rec)))
+	('inm `("(insert mode)" (global-erjoalgo-command-mode 0)))
+	;('nli `("<C-j>" (newline-and-indent)))
+	('nli `("\n" (newline-and-indent)))
+	('evl `(,(format "%s" arg1) ,arg1))
+	('cbd `("{\nREC\n}" (cbd)))
+	('cmt `(,(concat arg1 " <RET>")
+		,(when arg1 `(insert ,arg1))
+		(comint-send-input)))
+	('py-bck `(,"(unindent)"
+		   (python-indent-dedent-line-backspace nil )))
+	('py-scn `(": \n\t" (insert ":") (newline-and-indent)))
+	('scn `("; \n\t" (insert ";") (newline-and-indent)))
+	('idt `("(indent-for-tab-command)" (indent-for-tab-command)))
+	('cmo `("(comment-out current-prefix-arg)" (comment-out current-prefix-arg)))
+	('py-shift (let ((sym-name (format "python-indent-shift-%s" arg1)))
+		     (list sym-name (intern sym-name))))
+	('var-rec `(,(upcase (symbol-name arg1)) (buttons-recedit-record-text ',arg1)))
+	('var-pop `("" (buttons-pop-text ',arg1)))
+	('var-ins `(,(upcase (symbol-name arg1)) (insert (car ,arg1))))
+	(t (error "unknown action %s" action) ))
+      )))
+
+(defmacro buttons-make-bindings (language-prefix base-map &rest bindings)
+  (loop for (key-spec . actions) in bindings
+	with bindings = nil
+	as key = (buttons-add-super-modifier key-spec)
+	as descs = nil
+	as forms = (loop for action in actions
+			 as form-desc = (buttons-read-action action)
+			 append (destructuring-bind (desc . forms)
+				    form-desc
+				  (push desc descs)
+				  forms))
+	as name-sym = (gensym (format "buttons-%s-%s-" language-prefix
+				      key-spec))
+	as desc = (s-join "" (reverse descs))
+					;collect `(define-key ,kmap-sym ,key
+	collect `(cons ,key
+		       (defun ,name-sym ()
+			 ,desc (interactive)
+					;(let ()
+			 (let ((undo-len (length buffer-undo-list))
+			       (undo-inhibit-record-point t))
+			   (undo-boundary)
+			   (or (catch 'button-abort
+				 ,@forms t)
+			       (undo)))))
+	into bindings
+	finally (return `(append ,base-map (list ,@bindings)))))
+
+					;(defmacro with-temporary-erjoalgo-mode-state (state &body forms)
+(defmacro with-temporary-erjoalgo-mode-state (state &rest forms)
+  (let ((orig-state (gensym "erjoalgo-mode-orig-state-")))
+    `(let ((,orig-state erjoalgo-command-mode))
+       (global-erjoalgo-command-mode ,state)
+       ,@forms
+       (global-erjoalgo-command-mode ,orig-state))))
+
+(defun buttons-recedit-record-text (var-sym)
+  (with-temporary-erjoalgo-mode-state
+   0
+   (let ((old-point (point)))
+     (recursive-edit)
+     (when var-sym
+       (unless (boundp var-sym) (set var-sym nil))
+       (let ((text (buffer-substring-no-properties old-point (point))))
+	 (add-to-list var-sym text)
+	 '(set var-sym (cons text (symbol-value var-sym))))))))
+
+(defun buttons-pop-text (var-sym)
+  (set var-sym (cdr (symbol-value var-sym))))
+
+(setq cbd-map
+      (list 'go-mode 'js-mode 'js2-mode
+	    'c++-mode 'java-mode 'shell-script-mode 'sh-mode))
+
+(defun buttons-rec ()
+  (with-temporary-erjoalgo-mode-state 0 (recursive-edit)))
+
+(defun cbd (&optional content)
+  (insert "\t{")
+  (newline-and-indent)
+  (indent-for-tab-command)
+  (buttons-rec)
+  (newline-and-indent)
+  (insert "}")
+  (if (member major-mode '(c-mode js-mode))
+      (c-indent-line-or-region)
+    (indent-for-tab-command))
+  )
