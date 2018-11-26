44a4997e2d7b7bc7ac29c26465b6e52bc3b2e53f
Ernesto Alfonso <erjoalgo@gmail.com>
2018-11-26 15:55:27 -0800

    fix buttons.el relative path

--- test/test.sh
+++ test/test.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
-emacs -Q -batch -l ert -L .  \
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+emacs -Q -batch -l ert -L ..  \
       -l buttons-tests.el \
       -f ert-run-tests-batch-and-exit
