c8a4ec6fe735ac52646112857df65db146c02e39
Ernesto Alfonso <erjoalgo@gmail.com>
2018-11-28 22:52:04 -0800

    Revert "add failed attempt to batch flycheck"
    This reverts commit 03f80ddd4c0768c1f31e9f0794cf350fb526d948.

--- test/test.sh
+++ test/test.sh
@@ -11,7 +11,3 @@ test "${EMACS:-t}" = "t" && EMACS=emacs
 ${EMACS} -Q -batch -l ert -L ..  \
       -l buttons-tests.el \
       -f ert-run-tests-batch-and-exit
-
-# env
-# echo "${EMACS}"
-# ${EMACS:-emacs} -batch -L .. -l load-flycheck.el -l buttons.el -f flycheck-buffer
