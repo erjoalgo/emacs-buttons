629299c0d25e6e3cff4d047e50aabdfd6d9e2e78
Ernesto Alfonso <erjoalgo@gmail.com>
2018-11-28 22:51:32 -0800

    add failed attempt to batch flycheck

--- test/test.sh
+++ test/test.sh
@@ -11,3 +11,7 @@ test "${EMACS:-t}" = "t" && EMACS=emacs
 ${EMACS} -Q -batch -l ert -L ..  \
       -l buttons-tests.el \
       -f ert-run-tests-batch-and-exit
+
+# env
+# echo "${EMACS}"
+# ${EMACS:-emacs} -batch -L .. -l load-flycheck.el -l buttons.el -f flycheck-buffer
