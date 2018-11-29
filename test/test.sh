58ee3de699c4a2a05909d103edd4b495fe8cf30b
Ernesto Alfonso <erjoalgo@gmail.com>
2018-11-28 22:50:54 -0800

    check if EMACS is "t"

--- test/test.sh
+++ test/test.sh
@@ -6,6 +6,8 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 rm -f ../*.elc
 
-${EMACS:-emacs} -Q -batch -l ert -L ..  \
+test "${EMACS:-t}" = "t" && EMACS=emacs
+
+${EMACS} -Q -batch -l ert -L ..  \
       -l buttons-tests.el \
       -f ert-run-tests-batch-and-exit
