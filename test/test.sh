ea3c747ce5b2fb322b16982f1890b3cbf0354a1e
Ernesto Alfonso <erjoalgo@gmail.com>
2018-11-28 23:33:12 -0800

    move complicated --evals to a file

--- test/test.sh
+++ test/test.sh
@@ -12,7 +12,4 @@ ${EMACS} -Q -batch -l ert -L ..  \
       -l buttons-tests.el \
       -f ert-run-tests-batch-and-exit
 
-${EMACS} -Q --batch \
-         --eval '(with-demoted-errors "Error during package initialization: %S" (package-initialize) (package-refresh-contents) (package-install `flycheck))' \
-         --eval '(progn (defvar jka-compr-inhibit) (unwind-protect (let ((jka-compr-inhibit t)) (when (equal (car command-line-args-left) "--") (setq command-line-args-left (cdr command-line-args-left))) (defvar flycheck-byte-compiled-files nil) (let ((byte-compile-dest-file-function (lambda (source) (let ((temp-file (make-temp-file (file-name-nondirectory source)))) (push temp-file flycheck-byte-compiled-files) temp-file)))) (unwind-protect (byte-compile-file (car command-line-args-left)) (mapc (lambda (f) (ignore-errors (delete-file f))) flycheck-byte-compiled-files)) (when (bound-and-true-p flycheck-emacs-lisp-check-declare) (check-declare-file (car command-line-args-left))))) (setq command-line-args-left nil)))' \
-         -- ../buttons.el
+${EMACS} -Q --batch -l load-flycheck.el -- ../buttons.el
